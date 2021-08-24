858db5b9965fab0cbaa479cd70fea3ddf1ba160b
dufellipeR <eduardo.fellipe30@gmail.com>
2021-08-24 15:25:00 -0300

    exclude itens of show

--- src/modules/company_module/infra/typeorm/entities/CompanyModule.ts
+++ src/modules/company_module/infra/typeorm/entities/CompanyModule.ts
@@ -1,5 +1,6 @@
 import Company from '@modules/company/infra/typeorm/entities/Company';
 import Module from '@modules/module/infra/typeorm/entities/Module';
+import { Exclude } from 'class-transformer';
 import {
   Entity,
   Column,
@@ -10,9 +11,11 @@ import {
 
 @Entity('company_modules')
 class CompanyModule {
+  @Exclude()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Exclude()
   @Column()
   module_id: string;
 
@@ -20,9 +23,11 @@ class CompanyModule {
   @JoinColumn({ name: 'module_id' })
   module: Module;
 
+  @Exclude()
   @Column()
   company_id: string;
 
+  @Exclude()
   @ManyToOne(() => Company)
   @JoinColumn({ name: 'company_id' })
   company: Company;
