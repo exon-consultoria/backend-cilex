5c30af94e5a193d206ebf0f0f91cc76135031b26
dufellipeR <eduardo.fellipe30@gmail.com>
2021-08-24 18:06:35 -0300

    add eager module

--- src/modules/group_module/infra/typeorm/entities/GroupModule.ts
+++ src/modules/group_module/infra/typeorm/entities/GroupModule.ts
@@ -16,7 +16,7 @@ class GroupModule {
   @Column()
   module_id: string;
 
-  @ManyToOne(() => Module)
+  @ManyToOne(() => Module, { eager: true })
   @JoinColumn({ name: 'module_id' })
   module: Module;
 
