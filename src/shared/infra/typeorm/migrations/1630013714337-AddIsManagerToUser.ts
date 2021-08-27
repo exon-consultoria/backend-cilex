4fbf74b8ad05d9b444577b3b787863e1184deeba
dufellipeR <eduardo.fellipe30@gmail.com>
2021-08-27 16:25:33 -0300

    add isManagerColumn to User

---
+++ src/shared/infra/typeorm/migrations/1630013714337-AddIsManagerToUser.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class AddIsManagerToUser1630013714337
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'isManager',
+        type: 'boolean',
+        default: false,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'isManager');
+  }
+}
